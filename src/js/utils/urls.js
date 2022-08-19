752362a695409f640d82094bbfb5c8cb03d1bdf6
nikhil <jadhavnikhil86@gmail.com>
2022-08-19 21:29:28 +0530

    first commit

---
+++ src/js/utils/urls.js
@@ -0,0 +1,11 @@
+//************************* localhost urls *************************/
+
+// export const getAllFilesUrl = "http://localhost:5000/api/v1/files";
+// export const uploadFilesUrl = "http://localhost:5000/api/v1/upload";
+// export const getSingleFileUrl = "http://localhost:5000/api/v1/files/";
+
+//************************* production urls *************************/
+
+export const getAllFilesUrl = "https://csv-r.herokuapp.com/api/v1/files";
+export const uploadFilesUrl = "https://csv-r.herokuapp.com/api/v1/upload";
+export const getSingleFileUrl = "https://csv-r.herokuapp.com/api/v1/files/";
